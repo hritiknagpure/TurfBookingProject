07a8c9ca466073cff4065df4da70b133c356ac9e
hrithiknagpure <hritik.nagpure@francinaconsulting.com>
2025-04-30 12:37:19 +0530

    Add Pricerate column in EventBooking Table

--- LoginFormASPcore/Migrations/20250430070126_newcolumn.Designer.cs
+++ LoginFormASPcore/Migrations/20250430070126_newcolumn.Designer.cs
@@ -4,6 +4,7 @@ using LoginFormASPcore.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LoginFormASPcore.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250430070126_newcolumn")]
+    partial class newcolumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LoginFormASPcore/Migrations/20250430070126_newcolumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoginFormASPcore.Migrations
+{
+    /// <inheritdoc />
+    public partial class newcolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TicketPrice",
+                table: "EventBookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TicketPrice",
+                table: "EventBookings");
+        }
+    }
+}
